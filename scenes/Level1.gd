a8a273f53c206c0429d46920454e8de5387a4a71
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-02-29 11:49:56 +0700

    platform, win, lose, fish 50%

---
+++ scenes/Level1.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func trigger(scene,body):
+	if body.get_name() == "Player":
+		get_tree().change_scene(str("res://scenes/" + scene + ".tscn"))
+		
+
+func _on_LoseArea_body_entered(body):
+	trigger("LoseScreen",body)
+
+func _on_WinArea_body_entered(body):
+	trigger("WinScreen",body)
+
