36dae280b94525aaed67269b9838ae95501d3b44
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-04-17 20:04:39 +0700

    menu implemented

---
+++ scenes/button-menu.gd
@@ -0,0 +1,25 @@
+extends LinkButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+export(String) var scene_to_load
+
+func _on_new_pressed():
+	get_tree().change_scene(str("res://scenes/" + scene_to_load + ".tscn"))
+
+
+func _on_stage_pressed():
+	pass # Replace with function body.
